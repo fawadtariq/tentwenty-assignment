f34e498330537eb80dbb6e218fb50af27203ff45
Fawad Tariq <fawadtariqq@gmail.com>
2022-06-07 22:03:16 +0500

    theme-save

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  
+  <div class="grid grid-cols-9 pallete">
+    <div class="bg-primary"></div>
+    <div class="bg-secondary"></div>
+    <div class="bg-success"></div>
+    <div class="bg-info"></div>
+    <div class="bg-warning"></div>
+    <div class="bg-pending"></div>
+    <div class="bg-danger"></div>
+    <div class="bg-light"></div>
+    <div class="bg-dark"></div>
+  </div>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
@@ -17,5 +28,8 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.pallete > div{
+  width: 100px; height: 100px;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,53 @@
+const colors = require("tailwindcss/colors");
+const {
+  toRGB,
+  withOpacityValue,
+} = require("./src/helpers/tailwind-config-helper");
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // or 'media'
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+        rgb: toRGB(colors),
+        primary: withOpacityValue("--color-primary"),
+        secondary: withOpacityValue("--color-secondary"),
+        success: withOpacityValue("--color-success"),
+        info: withOpacityValue("--color-info"),
+        warning: withOpacityValue("--color-warning"),
+        pending: withOpacityValue("--color-pending"),
+        danger: withOpacityValue("--color-danger"),
+        light: withOpacityValue("--color-light"),
+        dark: withOpacityValue("--color-dark"),
+        slate: {
+          50: withOpacityValue("--color-slate-50"),
+          100: withOpacityValue("--color-slate-100"),
+          200: withOpacityValue("--color-slate-200"),
+          300: withOpacityValue("--color-slate-300"),
+          400: withOpacityValue("--color-slate-400"),
+          500: withOpacityValue("--color-slate-500"),
+          600: withOpacityValue("--color-slate-600"),
+          700: withOpacityValue("--color-slate-700"),
+          800: withOpacityValue("--color-slate-800"),
+          900: withOpacityValue("--color-slate-900"),
+        },
+        darkmode: {
+          50: withOpacityValue("--color-darkmode-50"),
+          100: withOpacityValue("--color-darkmode-100"),
+          200: withOpacityValue("--color-darkmode-200"),
+          300: withOpacityValue("--color-darkmode-300"),
+          400: withOpacityValue("--color-darkmode-400"),
+          500: withOpacityValue("--color-darkmode-500"),
+          600: withOpacityValue("--color-darkmode-600"),
+          700: withOpacityValue("--color-darkmode-700"),
+          800: withOpacityValue("--color-darkmode-800"),
+          900: withOpacityValue("--color-darkmode-900"),
+        },
+      },
+    },
   },
   plugins: [],
 }
